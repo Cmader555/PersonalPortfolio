abdc6948ce33ff15aa31b5b9276e2b1d308fe3d2
Christian Mader <Cmader555@users.noreply.github.com>
2019-04-29 09:26:56 -0700

    updated sylting and portfolio

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+$(function () {
+
+    $(document).scroll(function () {
+
+        var nav = $("#mainNavbar"); 
+        nav.toggleClass("scrolled", $(this).scrollTop() > nav.height()); 
+
+    }); 
+
+}); 
