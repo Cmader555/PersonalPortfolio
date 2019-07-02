7beaab6fcbfc993bcfbd27eea19ea410092f2e40
Cmader555 <46939703+Cmader555@users.noreply.github.com>
2019-07-01 22:06:10 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,44 +23,44 @@ $(".btn").on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", fu
 
 
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyChXWDqBQDbs4C2n3LX37eV9pvGcGlkjLo",
-    authDomain: "portfolio-d25a8.firebaseapp.com",
-    databaseURL: "https://portfolio-d25a8.firebaseio.com",
-    projectId: "portfolio-d25a8",
-    storageBucket: "portfolio-d25a8.appspot.com",
-    messagingSenderId: "396797600610",
-    appId: "1:396797600610:web:bd0be466967c1dc5"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// // Your web app's Firebase configuration
+// var firebaseConfig = {
+//     apiKey: "AIzaSyChXWDqBQDbs4C2n3LX37eV9pvGcGlkjLo",
+//     authDomain: "portfolio-d25a8.firebaseapp.com",
+//     databaseURL: "https://portfolio-d25a8.firebaseio.com",
+//     projectId: "portfolio-d25a8",
+//     storageBucket: "portfolio-d25a8.appspot.com",
+//     messagingSenderId: "396797600610",
+//     appId: "1:396797600610:web:bd0be466967c1dc5"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
 
-//setting firebase invoke to variable
+// //setting firebase invoke to variable
 
-var database = firebase.database();
+// var database = firebase.database();
 
-// stores contact info 
-$("#contact-submit").on("click", function (event) {
+// // stores contact info 
+// $("#contact-submit").on("click", function (event) {
 
-    event.preventDefault();
+//     event.preventDefault();
 
-    let contactName = $("#contactName").val().trim();
-    let contactEmail = $("#contactEmail").val().trim();
-    let contactMessage = $("#contactMessage").val().trim();
-
-
-    database.ref().push({
-
-        contactName: contactName,
-        contactEmail: contactEmail,
-        contactMessage: contactMessage
-
-    });
+//     let contactName = $("#contactName").val().trim();
+//     let contactEmail = $("#contactEmail").val().trim();
+//     let contactMessage = $("#contactMessage").val().trim();
 
 
-});
+//     database.ref().push({
+
+//         contactName: contactName,
+//         contactEmail: contactEmail,
+//         contactMessage: contactMessage
+
+//     });
+
+
+// });
 
 
 
